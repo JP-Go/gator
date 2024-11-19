44fc7c442f48d18f9600b73712210490853f9911
João Pedro <jpedro1580@gmail.com>
2024-11-18 22:48:21 -0300

    faet(cli): handle list users command

--- internal/command/handlers.go
+++ internal/command/handlers.go
@@ -5,5 +5,6 @@ func RegisterCommands() *commands {
 	cmds.Register("login", handleLogin)
 	cmds.Register("register", handleRegister)
 	cmds.Register("reset", handleReset)
+	cmds.Register("users", handleGetUsers)
 	return cmds
 }
